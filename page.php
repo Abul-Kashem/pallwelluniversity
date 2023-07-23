4df44372e0ddf80fc9acc232396a4fa6d659a729
Abul-Kashem <kashem.mcpd@gmail.com>
2023-07-23 15:59:31 +0600

    html file split header footer and functions file writing

--- page.php
+++ page.php
@@ -1,7 +1,14 @@
 <?php
-while (have_posts()):
-    the_post();
-endwhile;
+get_header();
 
+if (have_posts()): ?>
+    <?php while (have_posts()):
+        the_post(); ?>    
+        <h2><?php the_title(); ?></h2>
+        <?php the_content(); ?>
+    <?php endwhile; ?>
+<?php
+endif;
+get_footer();
 
 ?>
