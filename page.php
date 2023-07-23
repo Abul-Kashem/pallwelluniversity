e972ddd1db7142525da83dc22a8b3f6694e6b104
Abul-Kashem <kashem.mcpd@gmail.com>
2023-07-23 22:39:19 +0600

    Menu of child page working done.

--- page.php
+++ page.php
@@ -19,6 +19,7 @@ if (have_posts()) : ?>
 
     <div class="container container--narrow page-section">
 
+
       <?php
       $parent_id = wp_get_post_parent_id(get_the_ID());
       if ($parent_id) :
@@ -30,6 +31,33 @@ if (have_posts()) : ?>
           </p>
         </div>
 
+      <?php endif; ?>
+
+      <?php
+      $child_pages = get_pages(array(
+        'child_of' => get_the_ID()
+      ));
+
+      if ($parent_id or $child_pages) :
+      ?>
+        <div class="page-links">
+          <h2 class="page-links__title"><a href="<?php echo get_permalink($parent_id); ?>"><?php echo get_the_title($parent_id); ?></a></h2>
+          <ul class="min-list">
+            <?php
+            if ($parent_id) {
+              $findChildrenOf = $parent_id;
+            } else {
+              $findChildrenOf = get_the_ID();
+            }
+
+            wp_list_pages(array(
+              'title_li' => NULL,
+              'child_of' => $findChildrenOf,
+              'sort_column' => 'menu_order'
+            ));
+            ?>
+          </ul>
+        </div>
       <?php endif; ?>
 
 
