2097d732a9f596a62fc071d891535732db53cf40
Abul-Kashem <kashem.mcpd@gmail.com>
2023-07-24 08:53:28 +0600

    about-us current-menu-item class add

--- footer.php
+++ footer.php
@@ -13,18 +13,13 @@
            <h3 class="headline headline--small">Explore</h3>
            <nav class="nav-list">
 
-             <?php
-              wp_nav_menu(array(
-                'theme_location' => 'footer_one_location',
-              ));
-              ?>
 
-             <!-- <ul>
+             <ul>
                <li><a href="<?php echo site_url('/about-us'); ?>">About Us</a></li>
                <li><a href="#">Programs</a></li>
                <li><a href="#">Events</a></li>
                <li><a href="#">Campuses</a></li>
-             </ul> -->
+             </ul>
            </nav>
          </div>
 
@@ -32,17 +27,13 @@
            <h3 class="headline headline--small">Learn</h3>
            <nav class="nav-list">
 
-             <?php
-              wp_nav_menu(array(
-                'theme_location' => 'footer_two_location',
-              ));
-              ?>
-<!-- 
+
+
              <ul>
                <li><a href="#">Legal</a></li>
                <li><a href="<?php echo site_url('/privacy-policy'); ?>">Privacy</a></li>
                <li><a href="#">Careers</a></li>
-             </ul> -->
+             </ul>
            </nav>
          </div>
        </div>
