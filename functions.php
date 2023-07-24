7f1e1558a1f81b4bc88d92d327d605c05b3ebcfb
Abul-Kashem <kashem.mcpd@gmail.com>
2023-07-24 08:39:55 +0600

    wp_nav_menu task complete

--- functions.php
+++ functions.php
@@ -14,5 +14,14 @@ add_action('wp_enqueue_scripts', 'pallwell_university_files');
 function pallwell_university_features()
 {
     add_theme_support('title');
+    register_nav_menu('header_menu_location', 'Header Menu Location');
+    register_nav_menu('footer_one_location', 'Footer One Location');
+    register_nav_menu('footer_two_location', 'Footer Two Location');
+
+    // register_nav_menus(array(
+    //     'primary-menu'   => esc_html__('Primary Menu', 'pallwell'),
+    //     'secondary-menu' => esc_html__('Secondary Menu', 'pallwell'),
+    //     // Add more menu locations here if needed
+    // ));
 }
-add_action('after_theme_setup', 'pallwell_university_features');
+add_action('after_setup_theme', 'pallwell_university_features');
