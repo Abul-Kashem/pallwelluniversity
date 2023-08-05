56a47e3e30b4c108948ed247a27b988f1bafe0da
Abul-Kashem <kashem.mcpd@gmail.com>
2023-08-05 14:22:22 +0600

    Javascript & jQuery file add for search functionality

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('jquery'), 'version' => '9d65cb8b6a96584c237b');
--- functions.php
+++ functions.php
@@ -2,8 +2,8 @@
 
 function pallwell_university_files()
 {
-    wp_enqueue_script('pallwell-university-js', get_theme_file_uri('js/index.js'), array('jquery'), '1.0', true);
-
+    // wp_enqueue_script('pallwell-university-js', get_theme_file_uri('js/index.js'), array('jquery'), '1.0', true);
+    wp_enqueue_script('pallwell-university-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
     wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
     wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     wp_enqueue_style('pallwell_main_style', get_theme_file_uri('/css/style-index.css'));
